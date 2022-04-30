8ff5adeb02227f9ca4a011d42559bfc3e908b84f
Apooo <89362456+Apooolline@users.noreply.github.com>
2022-04-30 11:47:19 +0000

    Ajout des différents jours

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ function load() {
 
   var avant_hier = document.createElement('div') // on crée un div et on l'assigne à la variable avant_hier
     avant_hier.className = 'petit' // on donne à ce div la classe "petit"
-    avant_hier.innerHTML += getDateString(data[0]) // le contenu de ce div = le jour au format JJ/MM/AAAA pour le premier jour de data
+    avant_hier.innerHTML = getDateString(data[0]) // le contenu de ce div = le jour au format JJ/MM/AAAA pour le premier jour de data
   
   timeline.appendChild(avant_hier) // on ajoute le div avant_hier à l'élément HTML qui porte l'ID "timeline"
+
+  var hier = document.createElement('div')
+    hier.className = 'moyen'
+    hier.innerHTML = getDateString(data[1])
   
-    
+  timeline.appendChild(hier)
+
+  var aujourd_hui = document.createElement('div')
+    aujourd_hui.className = 'grand'
+    aujourd_hui.innerHTML = getDateString(data[2])
+
+  timeline.appendChild(aujourd_hui)
+
+  var demain = document.createElement('div')
+    demain.className = 'moyen'
+    demain.innerHTML = getDateString(data[3])
+
+  timeline.appendChild(demain)
+
+  var apres_demain = document.createElement('div')
+    apres_demain.className = 'petit'
+    apres_demain.innerHTML = getDateString(data[4])
+
+  timeline.appendChild(apres_demain)
 }
 
 function getDateString(ce_jour) {
@@ -27,7 +49,7 @@ function getDateString(ce_jour) {
 
 let data = [
   {
-    day: 398442888742305,
+    day: 398442305,
     humor: 1
   },
   {
@@ -35,12 +57,16 @@ let data = [
     humor: 3
   },
   {
-    day: 39844288305,
+    day: 799999999999696,
     humor: 2
   },
   {
     day: 39882305,
     humor: 5
+  },
+  {
+    day: 398820000305,
+    humor: 4
   }
 ]
 
